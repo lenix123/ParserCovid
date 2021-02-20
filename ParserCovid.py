a153396e636d7d7c697757ce266387676ff4a755
lenix123 <70968472+lenix123@users.noreply.github.com>
2021-02-20 21:37:28 +0300

    Add files via upload
    Сreated a class that works with coronavirus statistics. Information is taken from google. Added 3 methods to the class: covid_globe() displays statistics on coronavirus around the world, covid_country(country) displays statistics on coronavirus in the country (the country name is passed as an argument), popular_five_countries() displays the top 5 countries by the number of cases.

---
+++ ParserCovid.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Covid:
+    url = 'https://www.google.com/search?q=%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0+' \
+          '%D0%BF%D0%BE+%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D1%83&oq=%D1%8' \
+          '1%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0+%D0%BF%D0%BE+%D0%BA%D0%BE%D1%80%D0%BE%' \
+          'D0%BD%D0%BE&aqs=chrome.3.69i57j0i10j0i10i433l2j0i10.11372j0j1&sourceid=chrome&ie=UTF-8'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                             '(KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.68'}
+
+    def __init__(self):
+        self.full_page = requests.get(self.url, headers=self.headers)
+        self.soup = BeautifulSoup(self.full_page.content, 'html.parser')
+
+    def covid_globe(self):
+        convert = self.soup.find_all('div', class_='o6Yscf')
+        items = convert[0].find_all('td', {'jsname': 'VBiLTe'})
+
+        info = []
+        for item in items:
+            info.append(item.find('span').get_text())
+        print('Случаи заболевания: ' + info[0] + "; " + "Выздоровело: " + info[1] + "; " + "Умерло: " + info[2])
+
+    def covid_country(self, name):
+        convert = self.soup.find_all('div', class_='iaUz9d')
+        countries = convert[0].find_all('tr', {'class': 'viwUIc'})
+
+        for country in countries:
+            if len(country.find_all('td', {'data-vfs': name})) > 0:
+                info = []
+                items = country.find_all('td')
+                for item in items:
+                    info.append(item.find('span').get_text())
+                print('Страна: ' + info[0] + '; ' + 'Случаи заболевания ' + info[1] + '; '
+                      + 'Выздоровело ' + info[2] + '; ' + 'Умерло ' + info[3])
+                break
+
+    def popular_five_countries(self):
+        convert = self.soup.find_all('div', class_='iaUz9d')
+        countries = convert[0].find_all('tr', {'class': 'viwUIc'})
+        for i in range(1, 6):
+            print(i, countries[i].find('span').get_text())
